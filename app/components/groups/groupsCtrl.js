e7ed23c0a8f4c33a608872207db0fd3ad085f542
ipc103 <iancandysemail@gmail.com>
2016-01-13 16:30:58 -0500

    change code to date, add to model

--- app/components/groups/groupsCtrl.js
+++ app/components/groups/groupsCtrl.js
@@ -1,12 +1,12 @@
 groupApp.controller('GroupsController', ['$stateParams', 'Group', function($stateParams, Group){
   this.groups = Group.query({cohort: $stateParams.id});
-  this.newGroup = new Group({cohort: $stateParams.id, group_types: "2", group_code: "1234"});
+  this.newGroup = new Group({cohort: $stateParams.id, group_types: "2"});
   this.createGroups = function(){
     var self = this;
     var id = $stateParams.id
-    this.newGroup.$save(function(){
+    this.newGroup.$save(function(response){
       self.groups = Group.query({cohort: id })
-      self.newGroup = new Group({cohort: $stateParams.id, group_types: "2", group_code: "1234"});;
+      self.newGroup = new Group({cohort: $stateParams.id, group_types: "2"});;
     })
   }
 
