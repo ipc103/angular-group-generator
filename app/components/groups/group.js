35b9b66c94978ed1d14a11b04f228d8e5848d17c
ipc103 <iancandysemail@gmail.com>
2016-01-14 10:25:07 -0500

    remove  configuration

--- app/components/groups/group.js
+++ app/components/groups/group.js
@@ -1,3 +1,3 @@
 groupApp.factory('Group', ['$resource', function($resource){
-  return $resource('http://localhost:3000/api/groups/:id',{group_code: new Date()}, {save: {method: 'POST', isArray: false}});
+  return $resource('http://localhost:3000/api/groups/:id',{group_code: new Date()});
 }])
